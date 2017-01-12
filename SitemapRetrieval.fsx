f73bc38daca69e850c441ab6ed4375aba1058810
Mark Gray <markgray.online@googlemail.com>
2017-01-12 21:11:00 +0000

    Added downloader

--- SitemapRetrieval.fsx
+++ SitemapRetrieval.fsx
@@ -6,7 +6,6 @@ module SitemapRetrieval =
   open Hopac
   open HttpFs.Client
   open FSharp.Data
-  open Ionic
 
   type Sitemap = XmlProvider<"""<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><sitemap><loc>http://www.mouseprice.com/SiteMapHandler/SiteMap1.xml.gz</loc></sitemap></sitemapindex>""">
     
@@ -16,7 +15,7 @@ module SitemapRetrieval =
     
     let robotsTxt = "robots.txt"
 
-    let robotLink = new Uri ( mousePrice, robotsTxt)
+    let robotLink = new Uri ( mousePrice, robotsTxt )
 
     let getContent uri =
       Request.createUrl Get uri
@@ -26,13 +25,13 @@ module SitemapRetrieval =
     let getRobotTxt robotUri =
       getContent robotUri
 
-    let parseTxt (content:string) =
-      let noComments (line:string) = line.[0] <> '#'
-      let removeEmptyLines (line:string) = line.Trim() <> ""
-      let toPairs (line:string) = 
-        let k::v::_ = (line.Trim()).Split([|": "|], StringSplitOptions.None) |> Seq.toList
-        k.Trim(),v.Trim()
-      content.Split('\n')
+    let parseTxt ( content:string ) =
+      let noComments ( line:string ) = line.[0] <> '#'
+      let removeEmptyLines ( line:string ) = line.Trim () <> ""
+      let toPairs ( line:string ) = 
+        let k::v::_ = ( line.Trim () ).Split( [|": "|], StringSplitOptions.None ) |> Seq.toList
+        k.Trim (),v.Trim ()
+      content.Split ( '\n' )
       |> Seq.filter noComments 
       |> Seq.filter removeEmptyLines
       |> Seq.map toPairs
@@ -41,7 +40,7 @@ module SitemapRetrieval =
       getContent siteMapUri
 
     let getSitemapFromXml xmlContent =
-      let sitemap = Sitemap.Parse(xmlContent)
+      let sitemap = Sitemap.Parse xmlContent
       sitemap
     
     let extractSitemapLink =
@@ -56,14 +55,54 @@ module SitemapRetrieval =
       >> getSitemapFromXml
       
     let result =
-      robotLink.ToString() 
+      robotLink.ToString () 
       |> extractSitemapLink
       |> getSitemapXml
       
     let locs = 
-      result.XElement.Descendants()
+      result.XElement.Descendants ()
       |> Seq.collect (fun x -> 
-            x.Elements() 
+            x.Elements () 
             |> Seq.filter (fun ele -> ele.Name.LocalName = "loc")
             |> Seq.map (fun ele -> ele.Value) )
     locs
+
+[<AutoOpen>]
+module Downloader =
+  open System
+  open System.IO
+  open System.Net
+
+  let executingDir () = Directory.GetCurrentDirectory ()
+  let download link =
+    let downloadDir = Path.Combine ( executingDir (), "downloads" )
+    if not <| Directory.Exists downloadDir then Directory.CreateDirectory ( downloadDir ) |> ignore
+    let uri = 
+      (Uri link)
+    let fileName () = 
+      match uri.Segments |> Seq.toList with
+      | "/"::_::[name] -> Some name
+      | _ -> None
+    match fileName () with
+    | Some loc ->
+        use cli = new WebClient ()
+        cli.AsyncDownloadFile ( uri, Path.Combine ( executingDir (), "downloads", loc ) ) |> Async.RunSynchronously
+    | None -> ()
+
+  let downloadGzips all =
+    all
+    |> Seq.iter download
+
+[<AutoOpen>]
+module Unzipper =
+  open Ionic
+
+  let unzip zip =
+    ()
+[<AutoOpen>]
+module MousePricePropertyGetter =
+  open SitemapRetrieval
+  open Downloader
+  open Unzipper
+
+  let getProperties () = getSiteMap () |> downloadGzips
